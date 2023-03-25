4638a42b1c4cbb1d9f4cff53a4ba2cc288678d06
jason <jason@hedmans.org>
2023-03-25 13:17:48 +0100

    update logo and head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -16,6 +16,7 @@ import {
 } from "@manahippo/aptos-wallet-adapter";
 
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -42,6 +43,12 @@ export default function App({ Component, pageProps }: AppProps) {
           autoConnect={true}
       >
           <ChakraProvider theme={theme}>
+              <Head>
+                  <title>Aptos Arena</title>
+                  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                  <meta name="description" content="2D arcade-style platform fighter for Aptos NFTs" />
+                  <link rel="icon" href="/favicon.ico" />
+              </Head>
               <Component {...pageProps} />
           </ChakraProvider>
     </WalletProvider>
