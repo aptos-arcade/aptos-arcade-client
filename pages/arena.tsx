9f47a97d497dffe0f4fb4209e29be829c693100e
jason <jason@hedmans.org>
2023-05-29 20:22:58 -0400

    remove fighters from arena page

--- pages/arena.tsx
+++ pages/arena.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
             subHeaderText={description}
             controls={arenaControls}
         >
-            <Fighters unload={async () => {}} />
+            {/*<Fighters unload={async () => {}} />*/}
         </GamePageLayout>
     )
 }
